04dd43e433b2717fbcbeb73576afa005213c03f5
Andrew Aposhian <aaposhian@mavenlink.com>
2021-03-31 12:55:36 -0600

    Remove BaseDownloader brick and aws gem
    - Remove seemingly extraneous requires for base downloader in brick.rb and pipeline.rb
    - The base downloader may be included to make it available to new, custom bricks that are scaffolded; however we do not need this functionality

--- lib/gutdata/bricks/brick.rb
+++ lib/gutdata/bricks/brick.rb
@@ -4,7 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-require_relative 'base_downloader'
 require_relative 'utils'
 
 require_relative 'middleware/middleware'
